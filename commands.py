6c177f994be280e1aa7759eb72f020ff2b3dae4a
Lincoln Edsall <lnclndsll@gmail.com>
2026-01-25 15:50:36 -0500

    assign family messages

--- commands.py
+++ commands.py
@@ -1213,7 +1213,7 @@ async def update_tasks(client):
             worksheet.insert_row([str(row_data['id']), row_data['value']], next_row_index)
             next_row_index += 1
     
-# @command_handler.Loop(hours=3)
+@command_handler.Loop(hours=3)
 async def assign_some_families(client):
     # if not chance(5):
     #     return
