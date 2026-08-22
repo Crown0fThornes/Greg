75dffe130aaa36f3cd80f3deac4cb368e36a60ca
Lincoln Edsall <lnclndsll@gmail.com>
2026-08-22 19:12:45 -0400

    Working Fair DB with faster tickets function

--- commands/summer_fest.py
+++ commands/summer_fest.py
@@ -19,6 +19,21 @@ LOCAL_TZ = datetime.timezone(datetime.timedelta(hours=LOCAL_UTC_OFFSET))
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+def is_task_complete(conn, member_id, task_num):
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT 1
+        FROM fair_task_completions
+        WHERE member_id = ?
+          AND task_num = ?
+        LIMIT 1
+        """,
+        (member_id, task_num)
+    )
+
+    return cursor.fetchone() is not None
 
 def insert_submission(conn, member_id, task_num, channel_id, message_id):
     """
@@ -117,8 +132,11 @@ def get_task_submissions(conn, member_id, task_num):
     ]
 
 @command_handler.Command(access_type=AccessType.PRIVILEGED)
-async def ticket_recount(activator: Neighbor, context: Context):
+async def ticket_recount(activator: Neighbor, context: Context, month = None, day = None):
     task_db_path = BASE_DIR / "data" / "fair_task_submissions.db"
+    
+    create_fair_task_tables(task_db_path)
+    
     with sqlite3.connect(task_db_path) as conn:
     
         guild = context.guild
@@ -140,8 +158,8 @@ async def ticket_recount(activator: Neighbor, context: Context):
             needs_pics = num_pics > 0
             needs_messages = num_words > 0 or num_messages > 0
             
-            start_month = task["start_month"]
-            start_day = task["start_day"]
+            start_month = month or task["start_month"]
+            start_day = day or task["start_day"]
                     
             channels_to_search = task["submission_channel"]
             if not isinstance(channels_to_search, list):
@@ -160,13 +178,29 @@ async def ticket_recount(activator: Neighbor, context: Context):
             needs_pics = num_pics > 0
             needs_messages = num_words > 0 or num_messages > 0
             
-            start_month = task["start_month"]
-            start_day = task["start_day"]
+            start_month = month or task["start_month"]
+            start_day = day or task["start_day"]
+                   
+            member_ids = get_members_with_task_submissions(conn, i)
                     
-            async for member in guild.fetch_members():
-                add_task_completions(conn, guild, i, member.id, num_pics, num_words, num_messages, start_month, start_day)
+            for member_id in member_ids:
+                await add_task_completions(conn, guild, i, member_id, num_pics, num_words, num_messages, start_month, start_day)
         
         await target_message.edit(content="Done!")
+        
+def get_members_with_task_submissions(conn, task_num):
+    cursor = conn.cursor()
+
+    cursor.execute(
+        """
+        SELECT DISTINCT member_id
+        FROM fair_task_submissions
+        WHERE task_num = ?
+        """,
+        (task_num,)
+    )
+
+    return [row[0] for row in cursor.fetchall()]
         
 async def add_task_completions(conn, guild, task_num, member_id, num_pics, num_words, num_messages, start_month, start_day):
     
@@ -183,7 +217,7 @@ async def add_task_completions(conn, guild, task_num, member_id, num_pics, num_w
     
     # does member exist?
     try:
-        member = guild.fetch_member(member_id)
+        member = await guild.fetch_member(member_id)
     except:
         return
     
@@ -203,20 +237,19 @@ async def add_task_completions(conn, guild, task_num, member_id, num_pics, num_w
             continue
         
         # check if message has been invalidated with 👻
-        if has_ghost_reaction_from_role(message, 648188387836166168):
+        if await has_ghost_reaction_from_role(message, 648188387836166168):
             continue
         
         message_count += 1
         word_count += len(message.content.split())
-        pic_count += sum(att.content_type and att.content_type.startswith("image/") for att in message.attachments)
+        has_image = sum(att.content_type and att.content_type.startswith("image/") for att in message.attachments)
+        pic_count += has_image
         
     # add or remove completion
-    if pic_count > num_pics and word_count > num_words and message_count > num_messages:
+    if pic_count >= num_pics and word_count >= num_words and message_count >= num_messages:
         insert_task_complete(conn,member_id,task_num)
     else:
         remove_task_complete(conn,member_id,task_num)
-        
-        
                     
 async def add_submissions_after_date(conn, guild, channel_ids, task_num, needs_pics, needs_messages, start_month, start_day):
     # cycle through submission channels
@@ -242,8 +275,131 @@ async def add_submissions_after_date(conn, guild, channel_ids, task_num, needs_p
             elif needs_pics and any(att.content_type and att.content_type.startswith("image/") for att in message.attachments):
                 insert_submission(conn, message.author.id, task_num, channel_id, message.id)
     
+@command_handler.Command(access_type=AccessType.PUBLIC)
+async def tickets(activator: Neighbor, context: Context):
+    
+    date = commands.remember("tickets_last_counted_Fair_2026")
+    month, day = (8, 22) if date is None else date
+    
+    task_db_path = BASE_DIR / "data" / "fair_task_submissions.db"
+    # with sqlite3.connect(task_db_path) as conn:
+    #     await ticket_recount(activator, context, month, day)
+        
+    if len(context.args) > 0:
+        try:
+            target = await context.guild.fetch_member(int(parse_mention((context.args[0]))))
+        except:
+            candidates = [x.nick for x in context.guild.members if x.nick is not None]
+            candidates.extend([x.name for x in context.guild.members if not x.nick or x.nick != x.name])
+            name, _ = best_string_match(context.args[0], [str(x) for x in candidates])
+            target = discord.utils.get(context.guild.members, display_name=name) or \
+                     discord.utils.get(context.guild.members, name=name)
+    else:
+        target = None
+        
+    target_member = context.author if target is None else target
+    target_is_author = True if target is None else False
+        
+    task_info_path = BASE_DIR / "lookups" / "fair_tasks.json"
+    with task_info_path.open("r", encoding="utf-8") as f:
+        task_info = json.load(f)
+        
+    final_report = []
+    tickets_accumulated = 0;
+    
+    set2_role = context.guild.get_role(1539861798960767027)
+    set3_role = context.guild.get_role(1539861822461579264)
+        
+    with sqlite3.connect(task_db_path) as conn:
+       
+        # Check all tasks!
+        for i, task in enumerate(task_info, start=1):
+            set_num = int(task["set_number"])
+            required_tickets = (set_num-1)*10
+            if tickets_accumulated < required_tickets:
+                print(tickets_accumulated)
+                print(required_tickets)
+                break;
+            if set_num == 2:
+                await target_member.add_roles(set2_role)
+            if set_num == 3:
+                await target_member.add_roles(set3_role)
+            
+            if is_task_complete(conn,target_member.id,i):
+                final_report.append(1)
+                tickets_accumulated += task["tickets"]
+            else:
+                channels_to_search = task["submission_channel"]
+                if not isinstance(channels_to_search, list):
+                    channels_to_search = [channels_to_search]
+                    
+                # cycle through submission channels
+                for channel_id in channels_to_search:
+                    cur_channel = await context.guild.fetch_channel(channel_id)
+                    
+                    pic_count = 0;
+                    word_count = 0;
+                    message_count = 0;
+                    
+                    # check all messages
+                    async for message in cur_channel.history(limit=None,oldest_first=False):
+                        
+                        # end search if message too old
+                        msg_time = message.created_at
+                        if msg_time.tzinfo is None:
+                            msg_time = msg_time.replace(tzinfo=datetime.timezone.utc)
+                        msg_time = msg_time.astimezone(LOCAL_TZ)
+                        month = msg_time.month
+                        if month <= 7:
+                            break
+                        
+                        # skip if wrong author
+                        if message.author.id != target_member.id:
+                            continue;
+                        
+                        # up counts
+                        message_count += 1;
+                        word_count += len(message.content.split())
+                        has_image = sum(att.content_type and att.content_type.startswith("image/") for att in message.attachments)
+                        pic_count += has_image
+                        
+                    if pic_count >= task["pic_count"] and word_count >= task["word_count"] and message_count >= task["message_count"]:
+                        final_report.append(1)
+                        tickets_accumulated += task["tickets"]
+                        break
+                else:
+                    final_report.append(0)
+    
 
-@command_handler.Command(access_type=AccessType.PUBLIC,desc="See your Fair progress!")
+    target_member = context.author if target is None else target
+    target_is_author = True if target is None else False
+    res = "# FF Fair Progress\n"
+    if target_is_author:
+        res += f"**You have collected {tickets_accumulated} <:blue_carnival_ticket:1246080867114422335>!**\n\n"
+    else:
+        res += f"{target_member.display_name} has collected {tickets_accumulated} <:blue_carnival_ticket:1246080867114422335>!\n\n"
+
+    for i,is_task_completed in enumerate(final_report):
+        task = task_info[i]
+        res += f"Task {i+1}: {task["name"]} {"✅" if is_task_completed else "❌"}\n"
+    
+    res += "\n:arrow_right: See the set 1 task board: ⁠<#1533137141498908875>\n"
+    if i > 16:
+        res += "\n:arrow_right: See the set 2 task board: ⁠<#1533137175774756874>\n"
+        res += ":arrow_right: See the set 3 task board: ⁠<#1533137197702713558>\n"
+
+    elif i > 8:
+        res += "\n:arrow_right: See the set 2 task board: ⁠<#1533137175774756874>\n"
+        res += f"Unlock more tasks with {20 - tickets_accumulated} more tickets!\n"
+
+    else:
+        res += f"Unlock more tasks with {10 - tickets_accumulated} more tickets!\n"
+    res += "\n:arrow_right: Tasks due <t:1788235140:R>"
+    
+    await context.send(res,reply=True)
+    
+
+# @command_handler.Command(access_type=AccessType.PUBLIC,desc="See your Fair progress!")
 async def tickets(activator: Neighbor, context: Context):
     
     guild = context.guild
@@ -403,3 +559,39 @@ def convert_mentions_to_text(context: Context, str):
         return str;
     except:
         pass;
+
+
+import sqlite3
+
+def create_fair_task_tables(db_path):
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS fair_task_completions (
+                member_id INTEGER NOT NULL,
+                task_num INTEGER NOT NULL,
+
+                PRIMARY KEY (member_id, task_num)
+            )
+            """
+        )
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS fair_task_submissions (
+                member_id INTEGER NOT NULL,
+                task_num INTEGER NOT NULL,
+                channel_id INTEGER NOT NULL,
+                message_id INTEGER NOT NULL,
+
+                PRIMARY KEY (
+                    member_id,
+                    task_num,
+                    channel_id,
+                    message_id
+                )
+            )
+            """
+        )
